a708b41babc926fa70f61f4a076c4c0966471e67
Mikhail <misabaranov8@gmail.com>
2025-11-18 02:33:48 +0400

    Скопировал содержимое ветки origin/Alexandr в ailyzer/employer/frontend

---
+++ employer/frontend/src/App.vue
@@ -0,0 +1,46 @@
+<script>
+    import Footer from './components/Footer.vue';
+    import Header from './components/Header.vue';
+    import MainPage from './components/MainPage.vue';
+    import Registration from './components/Registration.vue';
+    import Login from './components/Login.vue';
+
+    export default {
+        name: 'App',
+        components: {
+        Header,
+        Footer,
+        MainPage,
+        Login,
+        Registration
+    },
+    data() {
+        return {
+        currentPage: 'mainpage'
+        }
+    },
+    methods: {
+        changePage(page) {
+        this.currentPage = page
+        }
+    }
+    }
+</script>
+
+<template>
+    
+    <Header :currentPage="currentPage" @navigate="changePage"></Header>
+
+    <main class="main-content">
+        <MainPage v-if="currentPage === 'mainpage'" @navigate="changePage">  <</MainPage>
+        <Login v-else-if="currentPage === 'login'" @navigate="changePage"> </Login>
+        <Registration v-else-if="currentPage === 'registration'" @navigate="changePage"> </Registration>
+    </main>
+
+    <Footer></Footer>
+
+</template>
+
+<style scoped>
+
+</style>
