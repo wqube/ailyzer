1566f4ad710c6b7c74187e64ae0a6a8121516d65
Timur Siraev <siraev.tima91@gmail.com>
2025-11-18 21:53:22 +0400

    add candidate to database

--- employer/backend/app/api/routers/__init__.py
+++ employer/backend/app/api/routers/__init__.py
@@ -8,6 +8,7 @@ from api.routers.user import router as user_router
 from api.routers.vacancy import router as vacancy_router
 from api.routers.role import router as role_router
 from api.routers.auth import router as auth_router  # 👈 НОВЫЙ роутер
+from api.routers.candidates import router as candidates_router
 
 # Демо-роутеры (можно оставить для тестирования)
 # from api.routers.demo_auth.views import router as demo_auth_router
@@ -21,7 +22,7 @@ router.include_router(auth_router)      # /api/v1/auth/*
 router.include_router(vacancy_router)   # /api/v1/vacancies/*
 router.include_router(user_router)      # /api/v1/users/*
 router.include_router(role_router)      # /api/v1/role/*
-
+router.include_router(candidates_router) 
 # 👇 ДЕМО роутеры (для тестирования, можно удалить в проде)
 # demo_auth_router.include_router(demo_auth_jwt_router)
 # router.include_router(demo_auth_router)  # /api/v1/demo_auth/*
